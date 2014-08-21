2cf27bf5dbcac3a39b8383dfbb74cb44a19714d2
Ryan Zec <code@ryanzec.com>
2014-08-21 18:14:51 -0400

    removed commented out code

--- tree-directive.js
+++ tree-directive.js
@@ -24,97 +24,91 @@ angular.module('nag.tree')
       templateUrl: function(element, attributes) {
         return nagHelper.getTemplatePath('tree');
       },
-      link: //function(element, attributes, transclude) {
-        //return {
-        //  pre: function(scope, element, attributes) {
-        //  },
-        /*  post:*/ function(scope, element, attributes) {
-            $(element).addClass('tree');
+      link: function(scope, element, attributes) {
+        $(element).addClass('tree');
 
-            scope.options = nagDefaults.getOptions('tree', scope.options);
+        scope.options = nagDefaults.getOptions('tree', scope.options);
 
-            /**
-             * Instance of nagHelper
-             *
-             * @ngscope
-             * @property {object} nagHelper
-             */
-            scope.nagHelper = nagHelper;
+        /**
+         * Instance of nagHelper
+         *
+         * @ngscope
+         * @property {object} nagHelper
+         */
+        scope.nagHelper = nagHelper;
 
-            /**
-             * Function to perform when a tree is clicked on
-             *
-             * @ngscope
-             * @method treeClick
-             *
-             * @param {object} $event Event object relating to the action
-             */
-            scope.treeClick = function($event) {
-              $event.preventDefault();
-              $event.stopPropagation();
-              $($event.currentTarget).toggleClass('expanded');
-            };
+        /**
+         * Function to perform when a tree is clicked on
+         *
+         * @ngscope
+         * @method treeClick
+         *
+         * @param {object} $event Event object relating to the action
+         */
+        scope.treeClick = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          $($event.currentTarget).toggleClass('expanded');
+        };
 
-            /**
-             * Function to perform when a node is clicked on
-             *
-             * @ngscope
-             * @method nodeClick
-             *
-             * @param {object} $event Event object relating to the action
-             * @param {object} nodeData Node data
-             */
-            scope.leafClick = function($event, nodeData) {
-              $event.preventDefault();
-              $event.stopPropagation();
-            };
+        /**
+         * Function to perform when a node is clicked on
+         *
+         * @ngscope
+         * @method nodeClick
+         *
+         * @param {object} $event Event object relating to the action
+         * @param {object} nodeData Node data
+         */
+        scope.leafClick = function($event, nodeData) {
+          $event.preventDefault();
+          $event.stopPropagation();
+        };
 
-            /**
-             * Determine if the passed in data has nodes
-             *
-             * @ngscope
-             * @method hasNodes
-             *
-             * @param {object} data Data to look at for nodes
-             *
-             * @returns {boolean} Whether or not the passed in data has nodes
-             */
-            scope.hasNodes = function(nodeData) {
-              return (angular.isDefined(nodeData.nodes) && angular.isArray(nodeData.nodes));
-            };
+        /**
+         * Determine if the passed in data has nodes
+         *
+         * @ngscope
+         * @method hasNodes
+         *
+         * @param {object} data Data to look at for nodes
+         *
+         * @returns {boolean} Whether or not the passed in data has nodes
+         */
+        scope.hasNodes = function(nodeData) {
+          return (angular.isDefined(nodeData.nodes) && angular.isArray(nodeData.nodes));
+        };
 
-            scope.hasIcon = function(nodeData) {
-              if(scope.hasNodes(nodeData)) {
-                return (!!nodeData.expandedSvg || !!nodeData.collapsedSvg);
-              } else {
-                return !!nodeData.staticSvg;
-              }
-            };
-
-            scope.getIconPath = function(nodeData) {
-              var iconPath;
-
-              if(scope.hasNodes(nodeData)) {
-                iconPath = nodeData.expandedSvg;
-              } else if(!!nodeData.staticSvg) {
-                iconPath = nodeData.staticSvg;
-              }
-
-              return iconPath;
-            };
-
-            //TODO: research: this seems to work however I have not idea and it only gets called 2 times even though it can take a while to load large tree
-            var interval = setInterval(function() {
-              //console.log('test');
-              if(element.find('img').length > 0) {
-                //console.log('test2');
-                clearInterval(interval);
-                SVGInjector(element.find('img').get());
-              }
-            }, 0);
+        scope.hasIcon = function(nodeData) {
+          if(scope.hasNodes(nodeData)) {
+            return (!!nodeData.expandedSvg || !!nodeData.collapsedSvg);
+          } else {
+            return !!nodeData.staticSvg;
           }
-        //};
-      //}
+        };
+
+        scope.getIconPath = function(nodeData) {
+          var iconPath;
+
+          if(scope.hasNodes(nodeData)) {
+            iconPath = nodeData.expandedSvg;
+          } else if(!!nodeData.staticSvg) {
+            iconPath = nodeData.staticSvg;
+          }
+
+          return iconPath;
+        };
+
+        //TODO: research: this seems to work however I have not idea and it only gets called 2 times even though it can take a while to load large tree
+        var interval = setInterval(function() {
+          //console.log('test');
+          if(element.find('img').length > 0) {
+            //console.log('test2');
+            clearInterval(interval);
+            SVGInjector(element.find('img').get());
+          }
+        }, 0);
+      }
     };
   }
 ]);
